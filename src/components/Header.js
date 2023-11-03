f49e30298666b72e3659010de2f9985a3aed1959
chathuranga <dev.chathuranga@gmail.com>
2023-11-03 13:56:19 +0530

    banner update

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,17 @@
 function Home() {
     return (
 <>
-<div class="main_top_bar">
+<div className="main_top_bar">
                   <h1><figure><img src="images/top-logo.png"/></figure></h1>
-                  <ul class="navigation">
-                      <li><a href="#">Home <i class="fa fa-caret-down"></i></a>
-                        <ul class="sub-menu">
+                  <ul className="navigation">
+                      <li><a href="#">Home <i className="fa fa-caret-down"></i></a>
+                        <ul className="sub-menu">
                           <li><a href="index.html">main home</a></li>
                           <li><a href="index-02.html">Home 02</a></li>
                         </ul>
                       </li>
-                      <li><a href="#">Pages<i class="fa fa-caret-down"></i></a>
-                        <ul class="sub-menu">
+                      <li><a href="#">Pages<i className="fa fa-caret-down"></i></a>
+                        <ul className="sub-menu">
                            <li><a href="about-us.html">about us</a></li>
                            <li><a href="team-page.html">team page</a></li>
                            <li><a href="team-detail.html">team detail</a></li>
@@ -21,41 +21,41 @@ function Home() {
                            <li><a href="404-page.html">404 page</a></li>
                         </ul>
                       </li>
-                      <li><a href="#">Services<i class="fa fa-caret-down"></i></a>
-                        <ul class="sub-menu">
+                      <li><a href="#">Services<i className="fa fa-caret-down"></i></a>
+                        <ul className="sub-menu">
                            <li><a href="service-grid.html">service grid</a></li>
                            <li><a href="service-grid02.html">service 02</a></li>
                            <li><a href="service-grid03.html">service 03</a></li>
                            <li><a href="service-detail.html">service detail</a></li>
                         </ul>
                       </li>
-                      <li><a href="#">Blog <i class="fa fa-caret-down"></i></a>
-                         <ul class="sub-menu">
+                      <li><a href="#">Blog <i className="fa fa-caret-down"></i></a>
+                         <ul className="sub-menu">
                            <li><a href="blog-post.html">blog page</a></li>
                            <li><a href="blog-post-full.html">blog post </a></li>
                            <li><a href="blog-post-sidebar.html">blog sidebar</a></li>
                            <li><a href="blog-detail.html">blog detail</a></li>
                         </ul>
                       </li>
-                      <li><a href="#"> Shop<i class="fa fa-caret-down"></i></a>
-                         <ul class="sub-menu">
+                      <li><a href="#"> Shop<i className="fa fa-caret-down"></i></a>
+                         <ul className="sub-menu">
                            <li><a href="shop-page.html">shop page</a></li>
                            <li><a href="shop-detail.html">Shop detail</a></li>
                          </ul>
                         </li>
                       <li><a href="contact-us.html"> Contact Us</a></li>
                   </ul>
-                  <div id="kode-responsive-navigation" class="dl-menuwrapper">
-                    <button class="dl-trigger">Open Menu</button>
-                    <ul class="dl-menu">
-                      <li><a class="active" href="#">Home</a>
-                        <ul class="dl-submenu">
+                  <div id="kode-responsive-navigation" className="dl-menuwrapper">
+                    <button className="dl-trigger">Open Menu</button>
+                    <ul className="dl-menu">
+                      <li><a className="active" href="#">Home</a>
+                        <ul className="dl-submenu">
                              <li><a href="index.html">main home</a></li>
                              <li><a href="index-02.html">Home 02</a></li>
                         </ul>
                       </li>
-                      <li class="menu-item kode-parent-menu"><a href="#">Pages</a>
-                          <ul class="dl-submenu">
+                      <li className="menu-item kode-parent-menu"><a href="#">Pages</a>
+                          <ul className="dl-submenu">
                              <li><a href="about-us.html">about us</a></li>
                              <li><a href="team-page.html">team page</a></li>
                              <li><a href="team-detail.html">team detail</a></li>
@@ -65,24 +65,24 @@ function Home() {
                              <li><a href="appointment.html">appointment</a></li>
                           </ul>
                       </li>
-                      <li class="menu-item kode-parent-menu"><a href="#">Services</a>
-                        <ul class="dl-submenu">
+                      <li className="menu-item kode-parent-menu"><a href="#">Services</a>
+                        <ul className="dl-submenu">
                             <li><a href="service-grid.html">service grid</a></li>
                            <li><a href="service-grid02.html">service 02</a></li>
                            <li><a href="service-grid03.html">service 03</a></li>
                            <li><a href="service-detail.html">service detail</a></li>
                         </ul>
                       </li>
-                      <li class="menu-item kode-parent-menu"><a href="#">blog</a>
-                        <ul class="dl-submenu">
+                      <li className="menu-item kode-parent-menu"><a href="#">blog</a>
+                        <ul className="dl-submenu">
                             <li><a href="blog-post.html">blog page</a></li>
                            <li><a href="blog-post-full.html">blog post </a></li>
                            <li><a href="blog-post-sidebar.html">blog sidebar</a></li>
                            <li><a href="blog-detail.html">blog detail</a></li>
                         </ul>
                       </li>
-                      <li class="menu-item kode-parent-menu"><a href="#">Shop</a>
-                        <ul class="dl-submenu">
+                      <li className="menu-item kode-parent-menu"><a href="#">Shop</a>
+                        <ul className="dl-submenu">
                             <li><a href="shop-page.html">shop page</a></li>
                            <li><a href="shop-detail.html">Shop detail</a></li>
                         </ul>
@@ -90,7 +90,7 @@ function Home() {
                       <li><a href="contact-us.html">contact Us</a></li>
                     </ul>
                   </div>
-                  <a class="main_button hover-affect" href="appointment.html">Book Appointment</a>
+                  <a className="main_button hover-affect" href="appointment.html">Book Appointment</a>
               </div>
 </>
     );
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,17 +4,17 @@ function Home() {
         <div className="main-wrapper">
         <div className="main_header">
              <div className="custom-container-fluid">
-             <div class="main_top_bar">
-                  <h1><figure><img src="images/top-logo.png"/></figure></h1>
-                  <ul class="navigation">
-                      <li><a href="#">Home <i class="fa fa-caret-down"></i></a>
-                        <ul class="sub-menu">
+             <div className="main_top_bar">
+                  <h1><figure><img src="/assets/images/logo.png" style={{height:'80px',color:'#ffff'}}/></figure></h1>
+                  <ul className="navigation">
+                      <li><a href="#">Home <i className="fa fa-caret-down"></i></a>
+                        <ul className="sub-menu">
                           <li><a href="index.html">main home</a></li>
                           <li><a href="index-02.html">Home 02</a></li>
                         </ul>
                       </li>
-                      <li><a href="#">Pages<i class="fa fa-caret-down"></i></a>
-                        <ul class="sub-menu">
+                      <li><a href="#">Pages<i className="fa fa-caret-down"></i></a>
+                        <ul className="sub-menu">
                            <li><a href="about-us.html">about us</a></li>
                            <li><a href="team-page.html">team page</a></li>
                            <li><a href="team-detail.html">team detail</a></li>
@@ -23,41 +23,41 @@ function Home() {
                            <li><a href="404-page.html">404 page</a></li>
                         </ul>
                       </li>
-                      <li><a href="#">Services<i class="fa fa-caret-down"></i></a>
-                        <ul class="sub-menu">
+                      <li><a href="#">Services<i className="fa fa-caret-down"></i></a>
+                        <ul className="sub-menu">
                            <li><a href="service-grid.html">service grid</a></li>
                            <li><a href="service-grid02.html">service 02</a></li>
                            <li><a href="service-grid03.html">service 03</a></li>
                            <li><a href="service-detail.html">service detail</a></li>
                         </ul>
                       </li>
-                      <li><a href="#">Blog <i class="fa fa-caret-down"></i></a>
-                         <ul class="sub-menu">
+                      <li><a href="#">Blog <i className="fa fa-caret-down"></i></a>
+                         <ul className="sub-menu">
                            <li><a href="blog-post.html">blog page</a></li>
                            <li><a href="blog-post-full.html">blog post </a></li>
                            <li><a href="blog-post-sidebar.html">blog sidebar</a></li>
                            <li><a href="blog-detail.html">blog detail</a></li>
                         </ul>
                       </li>
-                      <li><a href="#"> Shop<i class="fa fa-caret-down"></i></a>
-                         <ul class="sub-menu">
+                      <li><a href="#"> Shop<i className="fa fa-caret-down"></i></a>
+                         <ul className="sub-menu">
                            <li><a href="shop-page.html">shop page</a></li>
                            <li><a href="shop-detail.html">Shop detail</a></li>
                          </ul>
                         </li>
                       <li><a href="contact-us.html"> Contact Us</a></li>
                   </ul>
-                  <div id="kode-responsive-navigation" class="dl-menuwrapper">
-                    <button class="dl-trigger">Open Menu</button>
-                    <ul class="dl-menu">
-                      <li><a class="active" href="#">Home</a>
-                        <ul class="dl-submenu">
+                  <div id="kode-responsive-navigation" className="dl-menuwrapper">
+                    <button className="dl-trigger">Open Menu</button>
+                    <ul className="dl-menu">
+                      <li><a className="active" href="#">Home</a>
+                        <ul className="dl-submenu">
                              <li><a href="index.html">main home</a></li>
                              <li><a href="index-02.html">Home 02</a></li>
                         </ul>
                       </li>
-                      <li class="menu-item kode-parent-menu"><a href="#">Pages</a>
-                          <ul class="dl-submenu">
+                      <li className="menu-item kode-parent-menu"><a href="#">Pages</a>
+                          <ul className="dl-submenu">
                              <li><a href="about-us.html">about us</a></li>
                              <li><a href="team-page.html">team page</a></li>
                              <li><a href="team-detail.html">team detail</a></li>
@@ -67,24 +67,24 @@ function Home() {
                              <li><a href="appointment.html">appointment</a></li>
                           </ul>
                       </li>
-                      <li class="menu-item kode-parent-menu"><a href="#">Services</a>
-                        <ul class="dl-submenu">
+                      <li className="menu-item kode-parent-menu"><a href="#">Services</a>
+                        <ul className="dl-submenu">
                             <li><a href="service-grid.html">service grid</a></li>
                            <li><a href="service-grid02.html">service 02</a></li>
                            <li><a href="service-grid03.html">service 03</a></li>
                            <li><a href="service-detail.html">service detail</a></li>
                         </ul>
                       </li>
-                      <li class="menu-item kode-parent-menu"><a href="#">blog</a>
-                        <ul class="dl-submenu">
+                      <li className="menu-item kode-parent-menu"><a href="#">blog</a>
+                        <ul className="dl-submenu">
                             <li><a href="blog-post.html">blog page</a></li>
                            <li><a href="blog-post-full.html">blog post </a></li>
                            <li><a href="blog-post-sidebar.html">blog sidebar</a></li>
                            <li><a href="blog-detail.html">blog detail</a></li>
                         </ul>
                       </li>
-                      <li class="menu-item kode-parent-menu"><a href="#">Shop</a>
-                        <ul class="dl-submenu">
+                      <li className="menu-item kode-parent-menu"><a href="#">Shop</a>
+                        <ul className="dl-submenu">
                             <li><a href="shop-page.html">shop page</a></li>
                            <li><a href="shop-detail.html">Shop detail</a></li>
                         </ul>
@@ -92,7 +92,7 @@ function Home() {
                       <li><a href="contact-us.html">contact Us</a></li>
                     </ul>
                   </div>
-                  <a class="main_button hover-affect" href="appointment.html">Book Appointment</a>
+                  <a className="main_button hover-affect" href="appointment.html">Book Appointment</a>
               </div>
              </div>   
          </div>
@@ -211,7 +211,7 @@ function Home() {
                      <div>
                        <div className="mian_banner_fig">
                          <figure>
-                            <img src="/assets/images/banner-fig01.png"/>
+                            <img src="/assets/images/banner.png"/>
                         </figure>
                        </div>
                      </div>
